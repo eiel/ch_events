71d388a184945e2f9260c67bfc927de8869e8ee5
Tomohiko Himura <eiel.hal@gmail.com>
2016-06-07 22:42:27 +0900

    標準出力をloggerを経由するように変更

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,21 @@
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContext
 
 object Main extends App with Routes {
   implicit val actorSystem: ActorSystem = ActorSystem("http")
+  implicit val ec: ExecutionContext = actorSystem.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   val interface = "0.0.0.0"
   val port = getPort.getOrElse(8080)
-  println(s"start server $interface:$port")
-  val binding = Http().bindAndHandle(routes, interface, port)
+  val logger = Logging(actorSystem, getClass)
+  val binding = Http().bindAndHandle(routes, interface, port).map { (s: ServerBinding) =>
+    logger.info(s"start server $interface:$port")
+  }
 
   def getPort: Option[Int] = sys.env.get("PORT").map(_.toInt)
 }
