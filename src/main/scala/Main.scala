c1003a37b41ab9f72bd38e47d9aca0868c2d8c2c
Tomohiko Himura <eiel.hal@gmail.com>
2016-06-12 11:49:16 +0900

    :art:設定ファイルから読み込みするように変更

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.concurrent.ExecutionContext
 
@@ -10,12 +11,11 @@ object Main extends App with Routes {
   implicit val actorSystem: ActorSystem = ActorSystem("http")
   implicit val ec: ExecutionContext = actorSystem.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  val interface = "0.0.0.0"
-  val port = getPort.getOrElse(8080)
+  val config: Config = ConfigFactory.load()
+  val interface = config.getString("app.interface")
+  val port = config.getInt("app.port")
   val logger = Logging(actorSystem, getClass)
   val binding = Http().bindAndHandle(routes, interface, port).map { (s: ServerBinding) =>
     logger.info(s"start server $interface:$port")
   }
-
-  def getPort: Option[Int] = sys.env.get("PORT").map(_.toInt)
 }
