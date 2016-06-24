7cc8bfac6e112ad48f9edc3e5ffa94a2034a32fe
Tomohiko Himura <eiel.hal@gmail.com>
2016-06-24 09:45:32 +0900

    craet IdModel

---
+++ src/main/scala/chatwork/model/Model.scala
@@ -0,0 +1,6 @@
+package chatwork.model
+
+case class RoomId(id: Long) extends AnyVal
+case class MessageId(id: Long) extends AnyVal
+case class TaskId(id: Long) extends AnyVal
+case class FileId(id: Long) extends AnyVal
